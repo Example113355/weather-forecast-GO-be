5b3541d7d23d632f56154a15af0a9ad9813a062f
TranSon <tson113355@gmail.com>
2024-09-23 15:10:56 +0700

    Add django-cors-headers to requirements.txt and enable CORS in settings.py

--- weather/settings.py
+++ weather/settings.py
@@ -55,9 +55,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'weather_api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -65,6 +67,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://weather-forecast-go.vercel.app/",
 ]
 
 ROOT_URLCONF = 'weather.urls'
